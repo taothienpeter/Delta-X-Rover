8eacf3e9e561e3e800dbf127d96dfb188aedd7d8
taothienpeter <taothien2909@gmail.com>
2024-07-29 20:47:00 +0700

    change

--- src/py_pubsub/py_pubsub/publisher_member_function.py
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -37,14 +37,8 @@ class MinimalPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-
     minimal_publisher = MinimalPublisher()
-
     rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
